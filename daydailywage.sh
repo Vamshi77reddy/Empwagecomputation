b406c26a8bdf63e817fb15252251b39f26e6bd65
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-01-05 14:27:55 +0530

    storing day dailiy and total wage

---
+++ daydailywage.sh
@@ -0,0 +1,21 @@
+function calculateWorkingHour() {
+	case $1 in
+		0)
+			workingHour=0;
+			;;
+
+		1)
+			workingHour=8;
+			;;
+		2)
+			workingHour=4;
+			;;
+	esac;
+	echo $workingHour;
+}
+
+perHourSalary=20;
+totalSalary=0;
+totalWorkingHour=0;
+day=1;
+
